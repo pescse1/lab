0dd34f0795081173c6503aad6ec5cd8c50fcd617
pescse1 <pesscamit@gmail.com>
2026-06-12 22:43:27 +0530

    Update prg8.R

--- DALAB,6th/prg8.R
+++ DALAB,6th/prg8.R
@@ -1,30 +1,21 @@
+install.packages("arules")
+install.packages("arulesViz")
+installed.packages("RColorBrewer")
 library(arules)
-
-SupMarTra<- read.csv("G:/SNACK.csv", header = T, colClasses = "factor")
-
-summary(SupMarTra)
-
-length(SupMarTra)
-
-dim(SupMarTra)
-
-View(SupMarTra)
-
-rules<- apriori(SupMarTra)
-
-rules<-apriori(SupMarTra, parameter=list( supp = .5, conf = .8))
-inspect(rules)
-
-rules<- apriori(SupMarTra, parameter=list(minlen = 2, maxlen = 5, supp=.5, conf=.8))
-inspect(rules)
-
-rules<- apriori(SupMarTra, parameter=list(minlen = 2, maxlen = 5, supp=.5, conf=.8), appearance  = list(none=c("I1=No","I2=No","I3=No","I4=No","I5=No")))
-inspect(rules)
-
-write(rules, file = "D:/data3.CSV", sep = ",")
-
 library(arulesViz)
+library(RColorBrewer)
+SupMarTra<-read.transactions("E:/
+Snakyy.csv",format="basket",sep=",")
+summary(SupMarTra)
+dim(SupMarTra)
+View(SupMarTra)
+rules=apriori(SupMarTra)
+inspect(rules)
+rules=apriori(SupMarTra, parameter = list(supp=.3,conf=.8))
+inspect(rules)
+rules=apriori(SupMarTra,parameter = list(supp=.5,conf=.8))
+inspect(rules)
 plot(rules)
-plot(rules, method = "grouped",measure = "confidence")
-plot(rules, method = "graph", measure = "confidence",control = list(type="items"))
+plot(rules,method = "graph")
+
 
