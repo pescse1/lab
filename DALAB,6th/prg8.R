a9fe1a3ea9cd1830a6e589d8f372c3a8a0c50109
pescse1 <pesscamit@gmail.com>
2026-06-13 09:40:23 +0530

    Update prg8.R

--- DALAB,6th/prg8.R
+++ DALAB,6th/prg8.R
@@ -18,4 +18,41 @@ inspect(rules)
 plot(rules)
 plot(rules,method = "graph")
 
+or
+
+install.packages("arules")
+install.packages("arulesViz")
+install.packages("RColorBrewer")
+
+library(arules)
+library(arulesViz)
+library(RColorBrewer)
+
+SupMarTra <- read.transactions(
+  "C:Downloads/Snakyy.csv",
+  format = "basket",
+  sep = ","
+)
+
+summary(SupMarTra)
+dim(SupMarTra)
+
+rules <- apriori(SupMarTra)
+inspect(rules)
+
+rules <- apriori(
+  SupMarTra,
+  parameter = list(supp = 0.3, conf = 0.8)
+)
+inspect(rules)
+
+rules <- apriori(
+  SupMarTra,
+  parameter = list(supp = 0.5, conf = 0.8)
+)
+inspect(rules)
+
+plot(rules)
+plot(rules, method = "graph")
+
 
